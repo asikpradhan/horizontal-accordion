33b8822046dba7af3422fb9453e5326f2c165873
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2021-05-13 11:11:41 +0545

    feat: changes for es5

--- docs/assets/horizontal-accordion.js
+++ docs/assets/horizontal-accordion.js
@@ -2,86 +2,110 @@
  * @author Asik <asikpradhan@gmail.com>
  *
  */
+'use strict';
 
-
-const inactivate = (node) => {
-    node.classList.remove("active");
-    node.querySelectorAll(":scope > .horizontal-accordion-fold--content").forEach(item => {
+function inactivateFold(fold) {
+    fold.classList.remove("active");
+    fold.querySelectorAll(":scope > .horizontal-accordion-fold--content").forEach(function (item) {
         item.setAttribute("aria-expanded", false);
         item.removeAttribute("aria-disabled");
     });
-};
-const activate = (node) => {
-    node.classList.add("active");
-    node.querySelectorAll(":scope > .horizontal-accordion-fold--content").forEach(item => {
+}
+
+function activateFold(fold) {
+    fold.classList.add("active");
+    fold.querySelectorAll(":scope > .horizontal-accordion-fold--content").forEach(function (item) {
         item.setAttribute("aria-expanded", true);
         item.setAttribute("aria-disabled", true);
-
     });
-};
+}
 
-const accordions = document.getElementsByClassName("horizontal-accordion");
+function inActivateAllFolds(foldButton) {
+    var accordionNode = foldButton.parentNode.parentNode;
+    accordionNode.querySelectorAll(":scope > .horizontal-accordion-fold").forEach(function (item) {
+        inactivateFold(item);
+    });
+}
 
-Array.from(accordions).forEach(accordion => {
-    Array.from(accordion.children).forEach((child, index) => {
+var foldColors = ['maroon', 'purple', 'green', 'blue', 'orange'];
+
+function addFoldColor(fold, index) {
+    var hasColor = false;
+    foldColors.forEach(function (foldColor) {
+        if (fold.classList.contains(foldColor))
+            hasColor = true;
+    });
+    if (!hasColor) {
+        var foldColor = foldColors[index % foldColors.length];
+        fold.classList.add(foldColor);
+        fold.querySelectorAll(":scope > .horizontal-accordion-fold--button")
+            .forEach(function (button) {
+                button.classList.add("button--" + foldColor);
+            });
+    }
+}
+
+function addFoldClickEvent(accordionButton) {
+    accordionButton.addEventListener("click", function (evt) {
+        inActivateAllFolds(evt.currentTarget);
+        var parentNode = evt.currentTarget.parentNode;
+        activateFold(parentNode);
+    });
+}
+
+var accordions = document.getElementsByClassName("horizontal-accordion");
+var accordionButtons = document.getElementsByClassName("horizontal-accordion-fold--button");
+
+Array.from(accordionButtons).forEach(function (accordionButton) {
+    addFoldClickEvent(accordionButton);
+});
+Array.from(accordions).forEach(function(accordion) {
+    Array.from(accordion.children).forEach(function (fold, index) {
         if (index !== 0) {
-            inactivate(child);
+            inactivateFold(fold);
         } else {
-            activate(child);
+            activateFold(fold);
         }
+        addFoldColor(fold, index);
     });
-    const triggers = Array.prototype.slice.call(accordion.querySelectorAll('.horizontal-accordion-fold--button'));
-    accordion.addEventListener('keydown', (event) => {
-        const target = event.target;
-        const key = event.which.toString();
-        const ctrlModifier = (event.ctrlKey && key.match(/33|34/));
+    var foldButtons = Array.prototype.slice.call(accordion.querySelectorAll('.horizontal-accordion-fold--button'));
+    accordion.addEventListener('keydown', function (event) {
+        var target = event.target;
+        var key = event.which.toString();
+        var ctrlModifier = (event.ctrlKey && key.match(/33|34/));
         if (target.classList.contains('horizontal-accordion-fold--button')) {
 
             if (key.match(/38|40/) || ctrlModifier) {
-                const index = triggers.indexOf(target);
-                const direction = (key.match(/34|40/)) ? 1 : -1;
-                const length = triggers.length;
-                const newIndex = (index + length + direction) % length;
+                var index = foldButtons.indexOf(target);
+                var direction = (key.match(/34|40/)) ? 1 : -1;
+                var length = foldButtons.length;
+                var newIndex = (index + length + direction) % length;
 
-                triggers[newIndex].focus();
-                inActivateAll(triggers[newIndex]);
-                activate(triggers[newIndex].parentNode);
+                foldButtons[newIndex].focus();
+                inActivateAllFolds(foldButtons[newIndex]);
+                activateFold(foldButtons[newIndex].parentNode);
                 event.preventDefault();
             } else if (key.match(/35|36/)) {
                 // 35 = End, 36 = Home keyboard operations
                 switch (key) {
                     // Go to first accordion
                     case '36':
-                        triggers[0].focus();
-                        inActivateAll(triggers[0]);
-                        activate(triggers[0].parentNode);
+                        foldButtons[0].focus();
+                        inActivateAllFolds(foldButtons[0]);
+                        activateFold(foldButtons[0].parentNode);
                         break;
                     // Go to last accordion
                     case '35':
-                        triggers[triggers.length - 1].focus();
-                        inActivateAll(triggers[triggers.length - 1]);
-                        activate(triggers[triggers.length - 1].parentNode);
+                        foldButtons[foldButtons.length - 1].focus();
+                        inActivateAllFolds(foldButtons[foldButtons.length - 1]);
+                        activateFold(foldButtons[foldButtons.length - 1].parentNode);
                         break;
                 }
                 event.preventDefault();
-
             }
         }
     });
 
 });
-const inActivateAll = (current) => {
-    const accordionNode = current.parentNode.parentNode;
-    accordionNode.querySelectorAll(":scope > .horizontal-accordion-fold").forEach(item => {
-        inactivate(item);
-    });
-};
 
-const accordionButtons = document.getElementsByClassName("horizontal-accordion-fold--button");
-Array.from(accordionButtons).forEach(accordionButton => {
-    accordionButton.addEventListener("click", evt => {
-        inActivateAll(evt.currentTarget);
-        const parentNode = evt.currentTarget.parentNode;
-        activate(parentNode);
-    });
-});
+
